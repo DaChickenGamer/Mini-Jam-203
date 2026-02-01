b121b8b33960b20dacfa7666b68b21308f342448
Gabriel Rodrigues <gabrielrodrigues155@gmail.com>
2026-02-01 03:23:04 +0000

    FishingRod and Fishing Sprite. Fish moving randomly based on difficulty

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -2,27 +2,43 @@ using UnityEngine;
 
 public class Fish
 {
-    public float fishToughness;
+    private int fishToughness;
+        private float[] toughnessArray = { 0.1f, 0.5f, 10f, 20f };
+
     public string name;
-    private float spawnLevel;
+    private GameObject fishObject;
+    private float currentLevel;
 
-
-    public Fish(float toughness, string fishName)
+    public Fish(int toughness, string fishName, Sprite fishSprite)
     {
-        fishToughness = toughness;
-        name = fishName;
+        fishObject = new GameObject("Fish_" + name);
+        var sr = fishObject.AddComponent<SpriteRenderer>();
+        sr.sprite = fishSprite;
+        sr.sortingOrder = 10;
         Spawn(FishingMiniGame.maxCapacity);
     }
-
+        
     public void Spawn(float maxCapacity)
     {
         float level = Random.Range(0f, maxCapacity);
         Debug.Log("Fish " + name + " spawned at level: " + level);
-        spawnLevel = level;
+        currentLevel = level;
+        fishObject.transform.position = new Vector3(0, currentLevel, 0);
     }
 
-    public float GetSpawnLevel()
+    public void NewSpawn()
     {
-        return spawnLevel;
+        if( Random.Range(0f, 1f) < toughnessArray[fishToughness] / 100f )
+        {
+            Spawn(FishingMiniGame.maxCapacity);
+            Debug.Log("Fish " + name + " respawned at level: " + currentLevel);
+        }
+       
+    }
+
+    public float GetCurrentLevel()
+    {
+        Debug.Log("Current Level called, level: " + currentLevel);
+        return currentLevel ;
     }
 }
--- Assets/Scripts/FishingMiniGame.cs
+++ Assets/Scripts/FishingMiniGame.cs
@@ -2,41 +2,52 @@ using UnityEngine;
 
 public class FishingMiniGame : MonoBehaviour
 {
-    public static float maxCapacity;
+
+    public GameObject fishingRod;
+    public Sprite fishSprite;  
+
+    private GameObject maxCapacityRod;
+    public static float maxCapacity = 10f;
     public float rodStrength;
     private float currentLevel;
-    private float min,max;
-    private const float movingRate = 1f; 
-    Fish fish1;  
+    private float min, max;
+    private const float movingRate = 1f;
+    Fish fish1;
     void Start()
     {
-        maxCapacity = 100f;
-        rodStrength = 10f;
-        currentLevel= maxCapacity / 2;
-        CalculateRange();
 
-        fish1 = new Fish(5f, "cod");
-    
+        currentLevel = maxCapacity / 2;
+        CalculateRange();
+        CreateBars();
+        fish1 = new Fish(1, "cod", fishSprite);
+        Debug.Log("FishingRod: " + fishingRod);
     }
 
-    
+
+
     void Update()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            currentLevel+= movingRate * Time.deltaTime;
-          
-            
-        }else
-        {
-            currentLevel-= movingRate * Time.deltaTime;
-           
+            currentLevel += movingRate * Time.deltaTime;
+
+
         }
-          CalculateRange();
-          if(IsFishCaught(fish1.GetSpawnLevel()))
-          {
+        else
+        {
+            currentLevel -= movingRate * Time.deltaTime;
+
+        }
+        currentLevel = Mathf.Clamp(currentLevel, 0+rodStrength/2, maxCapacity-rodStrength/2);
+        CalculateRange();
+        if (IsFishCaught(fish1.GetCurrentLevel()))
+        {
             Debug.Log("Fish Caught: " + fish1.name);
-          }
+        }
+        else
+        {
+            fish1.NewSpawn();
+        }
     }
 
 
@@ -44,12 +55,28 @@ public class FishingMiniGame : MonoBehaviour
     {
         min = currentLevel - rodStrength;
         max = currentLevel + rodStrength;
-        Debug.Log("Min: " + min + " Max: " + max);
+        fishingRod.transform.position = new Vector3(0, currentLevel, -0.1f);
+
+        // Debug.Log("Min: " + min + " Max: " + max);
     }
 
     bool IsFishCaught(float fishLevel)
     {
         return fishLevel >= min && fishLevel <= max;
+    }
+
+
+    void CreateBars()
+    {
+
+        maxCapacityRod = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        maxCapacityRod.transform.position = new Vector3(0, maxCapacity / 2, 0);
+        maxCapacityRod.transform.localScale = new Vector3(1f, maxCapacity, 1f);
+
+
+
+        fishingRod.transform.position = new Vector3(0, 0, -0.1f);
+        fishingRod.transform.localScale = new Vector3(1f, rodStrength, 1f);
     }
 
 }
